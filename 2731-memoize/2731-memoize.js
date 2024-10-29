70b2a99788b3eeef7cc3d687334384e7fdfa8589
Getachew Muhabaw <93621993+Getachew-Muhabaw-Mesfin@users.noreply.github.com>
2024-10-29 12:10:55 +0300

    Time: 314 ms (33.36%), Space: 86.8 MB (77.58%) - LeetHub

--- 2731-memoize/2731-memoize.js
+++ 2731-memoize/2731-memoize.js
@@ -2,22 +2,39 @@
  * @param {Function} fn
  * @return {Function}
  */
-var memoize = function(fn) {
-    const cache = new Map();  
+// var memoize = function(fn) {
+//     const cache = new Map();  
 
-    return function(...args) {
-        const key = JSON.stringify(args);  
+//     return function(...args) {
+//         const key = JSON.stringify(args);  
         
-        if (cache.has(key)) {
-            return cache.get(key);  
-        }
+//         if (cache.has(key)) {
+//             return cache.get(key);  
+//         }
 
-        const result = fn(...args); 
-        cache.set(key, result); 
-        return result;
-    };
-};
-
+//         const result = fn(...args); 
+//         cache.set(key, result); 
+//         return result;
+//     };
+// };
+function memoize(fn) {
+    
+   const cache = {};
+  
+   return function(...args) {
+    const key = JSON.stringify(args);
+    
+    if (key in cache) {
+      return cache[key];
+    }
+    
+    const result = fn.apply(this, args);
+    cache[key] = result;
+    
+    return result;
+  }
+  
+}
 
 /** 
  * let callCount = 0;
